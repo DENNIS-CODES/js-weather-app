8cc42943fc4369b3c862d7e287a5b93b5728c500
edieatha <edieatha@gmail.com>
2020-10-22 15:39:33 +0300

    Add city search box and query on keypress

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ const api = {
     key: "74b515b6adf05a133d5641ca29abaa14",
     base: "https://api.openweathermap.org/data/2.5/",
 }
+
+const searchBox = document.querySelector('.search-box');
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery (event) {
+    if (event.keyCode === 13){
+        getResults(searchBox.value);
+    }
+}
