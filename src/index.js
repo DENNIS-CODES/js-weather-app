6e63d4d33974eba2ec7612c7e218062bd6a12165
edieatha <edieatha@gmail.com>
2020-10-22 15:42:18 +0300

    Add dateBuilder function and window.onload current date.

--- src/index.js
+++ src/index.js
@@ -18,3 +18,20 @@ function getResults (query) {
     }).then(displayResults)
 }
 
+window.onload = function () {
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+}
+
+function  dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+}
